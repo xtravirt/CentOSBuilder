f01787fec264a5dfa2127e1eb64a6b8b6d2b196d
Paul E Davey <paul.davey@xtravirt.com>
2018-09-06 18:58:02 +0100

    Still trying to get late_command to work!

--- builder.sh
+++ builder.sh
@@ -2,58 +2,53 @@
 set -e
 
 # set defaults
-default_hostname="$(hostname)"
-default_domain="sanlan"
-default_puppetmaster="puppetmaster.sanlan"
-default_puppet="n"
-tmp="/root/"
-username="$(logname)"
-authorized_keyfile="http://fileserver.sanlan:80/my-machines.pub"
-
-clear
+#default_hostname="$(hostname)"
+#default_domain="sanlan"
+#tmp="/root/"
+#username="$(logname)"
 
 # check for root privilege
-if [ "$(id -u)" != "0" ]; then
-   echo " this script must be run as root" 1>&2
-   echo
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+ #  echo " this script must be run as root" 1>&2
+  # echo
+   #exit 1
+#fi
 
 # define download function
 # courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
-download()
-{
-    local url=$1
-    echo -n "    "
-    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
-        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
-    echo -ne "\b\b\b\b"
-    echo " DONE"
-}
+#download()
+##{
+#    local url=$1
+#    echo -n "    "
+#    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+#        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+#    echo -ne "\b\b\b\b"
+#    echo " DONE"
+#}
 
 # determine ubuntu version
-ubuntu_version=$(lsb_release -cs)
+#ubuntu_version=$(lsb_release -cs)
 
 # check for interactive shell
-if ! grep -q "noninteractive" /proc/cmdline ; then
-    stty sane
-
-    # ask questions
-    read -ep " please enter your preferred hostname: " -i "$default_hostname" hostname
-    read -ep " please enter your preferred domain: " -i "$default_domain" domain
-fi
+#if ! grep -q "noninteractive" /proc/cmdline ; then
+#    stty sane
+#
+ #   # ask questions
+  #  read -ep " please enter your preferred hostname: " -i "$default_hostname" hostname
+   # read -ep " please enter your preferred domain: " -i "$default_domain" domain
+#fi
 
 # print status message
-echo " Preparing server. This may take a few minutes"
+#echo " Preparing server. This may take a few minutes"
 
 # set fqdn
-fqdn="$hostname.$domain"
+#fqdn="$hostname.$domain"
 
 # update hostname
-echo "$hostname" > /etc/hostname
-sed -i "s@ubuntu.ubuntu@$fqdn@g" /etc/hosts
-sed -i "s@ubuntu@$hostname@g" /etc/hosts
-hostname "$hostname"
+#echo "$hostname" > /etc/hostname
+#sed -i "s@ubuntu.ubuntu@$fqdn@g" /etc/hosts
+#sed -i "s@ubuntu@$hostname@g" /etc/hosts
+#hostname "$hostname"
 
 # update repos
 apt-get -y update
@@ -108,10 +103,10 @@ apt-get -y autoremove
 apt-get -y purge
 
 # remove myself to prevent any unintended changes at a later stage
-rm $0
+#rm $0
 
 # finish
-echo " DONE; rebooting ... "
+#echo " DONE; rebooting ... "
 
 # reboot
 reboot
