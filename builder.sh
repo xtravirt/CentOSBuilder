42edd333865e0fc1cb923db308afc121d716d7f0
sam-perrin <38654228+sam-perrin@users.noreply.github.com>
2018-09-18 16:01:02 +0100

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -9,8 +9,8 @@ apt-get -y -o Dpkg::Options::="--force-confold" upgrade
 
 # Installation of Jenkins
 echo "Installing Jenkins"
-cd /tmp
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+cd "/tmp"
+wget -q -O - "https://pkg.jenkins.io/debian/jenkins-ci.org.key" | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install jenkins -y
@@ -19,25 +19,20 @@ systemctl start jenkins
 
 # Installation of Packer
 echo "Create Packer Directory & Configure Permissions"
-mkdir /opt/packer
-
-# For testing with target
-mkdir /target/opt/packer
-touch /target/opt/packer/thisfile.test
-
-chmod 777 /opt/packer
-cd /tmp
+mkdir "/opt/packer"
+chmod 777 "/opt/packer"
+cd "/tmp"
 echo "Download packer_1.2.5_linux_amd64"
-wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+wget "https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip"
 echo "Extract and place packer"
-unzip packer_1.2.5_linux_amd64.zip -d /opt/packer
+unzip packer_1.2.5_linux_amd64.zip -d "/opt/packer"
 
 # Download Packer scripts from Sonar Github Account
 echo "Download custom packer scripts and locate"
-cd /tmp
-wget https://github.com/xtravirt/packer/archive/master.zip
-unzip master.zip -d /tmp
-mv /tmp/packer-master/* /opt/packer
+cd "/tmp"
+wget "https://github.com/xtravirt/packer/archive/master.zip"
+unzip master.zip -d "/tmp"
+mv /tmp/packer-master/* "/opt/packer"
 
 # Configure variables
 echo "Update Environment"
